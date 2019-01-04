f0bf6824419b53e6783cfed9962baf3780112390
Tommy Slaatbraaten <tommy.slaatbraaten@hotmail.com>
2019-01-04 15:04:21 +0100

    can connect to a vpn, reads the phonebook on the client pc and lists the entries on the tray icon when right clicked. left clicking on a entry connects or disconnects the user from the vpn

--- VPN_Switch/MainWindow.xaml.cs
+++ VPN_Switch/MainWindow.xaml.cs
@@ -15,6 +15,13 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
 using System.Drawing;
+using Hardcodet.Wpf.TaskbarNotification;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using DotRas;
+using System.Net.NetworkInformation;
+using MessageBox = System.Windows.MessageBox;
 
 namespace VPN_Switch
 {
@@ -23,28 +30,62 @@ namespace VPN_Switch
     /// </summary>
     public partial class MainWindow : Window
     {
+        public bool isConnected = false;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            NotifyIcon ni = new NotifyIcon();
-            ni.Icon = new Icon("shield.ico");
-            ni.Visible = true;
-            ni.DoubleClick +=
-                delegate (object sender, EventArgs args)
+            lbl_CurrentIP.Content = "Current ip is: " + GetLocalIPAddress();
+
+            ReadPhonebook();
+        }
+
+        private bool CheckConnection()
+        {
+            if (NetworkInterface.GetIsNetworkAvailable())
+            {
+                NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+                foreach (NetworkInterface Interface in interfaces)
                 {
-                    this.Show();
-                    this.WindowState = System.Windows.WindowState.Normal;
-                };
+                    if (Interface.OperationalStatus == OperationalStatus.Up)
+                    {
+                        if ((Interface.NetworkInterfaceType == NetworkInterfaceType.Ppp) && (Interface.NetworkInterfaceType != NetworkInterfaceType.Loopback))
+                        {
+                            IPv4InterfaceStatistics statistics = Interface.GetIPv4Statistics();
+                            MessageBox.Show(Interface.Name + " " + Interface.NetworkInterfaceType.ToString() + " " + Interface.Description);
+                            return true;
+                        }
+                    }
+                }
+                //MessageBox.Show("VPN Connection is lost!");
+            }
+            return false;
+        }
+
+        private void ShowWindow_Clicked(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Visible;
+        }
+
+        private void OpenConnection()
+        {
+            Process.Start(@"rasdial.exe", "hhm-vpn " + txt_username.Text + " " + txt_password.Text);
+            lbl_CurrentIP.Content = "Current ip is: " + GetLocalIPAddress();
+        }
+
+        private void CloseConnection()
+        {
+            Process.Start(@"rasdial.exe", "hhm-vpn /d");
         }
 
         // minimize to system tray when applicaiton is minimized
-        protected override void OnStateChanged(EventArgs e)
-        {
-            if (WindowState == WindowState.Minimized) this.Hide();
+        //protected override void OnStateChanged(EventArgs e)
+        //{
+        //    if (WindowState == WindowState.Minimized) this.Hide();
 
-            base.OnStateChanged(e);
-        }
+        //    base.OnStateChanged(e);
+        //}
 
         // minimize to system tray when applicaiton is closed
         protected override void OnClosing(CancelEventArgs e)
@@ -56,6 +97,59 @@ namespace VPN_Switch
             this.Hide();
 
             base.OnClosing(e);
+        }
+
+        private void Connect_Clicked(object sender, RoutedEventArgs e)
+        {
+            //Check if connected to vpn, if true, disconnect, if false, connect to vpn
+            if (CheckConnection())
+            {
+                CloseConnection();
+            }
+            else
+            {
+                OpenConnection();
+            }
+        }
+
+        private void ReadPhonebook()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) +
+                          @"\Microsoft\Network\Connections\Pbk\rasphone.pbk";
+
+            RasPhoneBook pbk = new RasPhoneBook();
+            pbk.Open(path);
+
+            foreach (RasEntry entry in pbk.Entries)
+            {
+                System.Windows.Controls.MenuItem item = new System.Windows.Controls.MenuItem();
+                item.Click += Connect_Clicked;
+                item.Header = entry.Name;
+                TbI.ContextMenu.Items.Add(item);
+            }
+        }
+
+        private static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private void Disconnect_clicked(object sender, RoutedEventArgs e)
+        {
+            CloseConnection();
+        }
+
+        private void Exit_Clicked(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
         }
     }
 }
