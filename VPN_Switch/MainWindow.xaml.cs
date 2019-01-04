ec8a01b2f08d2954cdc5a8a8bdaec6367ae60eb4
Tommy Slaatbraaten <tommy.slaatbraaten@hotmail.com>
2019-01-04 09:44:26 +0100

    begining

--- VPN_Switch/MainWindow.xaml.cs
+++ VPN_Switch/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace VPN_Switch
 {
@@ -23,6 +26,35 @@ namespace VPN_Switch
         public MainWindow()
         {
             InitializeComponent();
+            NotifyIcon ni = new NotifyIcon();
+            ni.Icon = new Icon("appIcon.ico");
+            ni.Visible = true;
+            ni.DoubleClick +=
+                delegate (object sender, EventArgs args)
+                {
+                    this.Show();
+                    this.WindowState = System.Windows.WindowState.Normal;
+                };
+        }
+
+        // minimize to system tray when applicaiton is minimized
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized) this.Hide();
+
+            base.OnStateChanged(e);
+        }
+
+        // minimize to system tray when applicaiton is closed
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            // setting cancel to true will cancel the close request
+            // so the application is not closed
+            e.Cancel = true;
+
+            this.Hide();
+
+            base.OnClosing(e);
         }
     }
 }
